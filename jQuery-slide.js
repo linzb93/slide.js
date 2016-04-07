e103c35298139824eb07a045bb6fe75477e9d021
林蛤蛤 <linzb93@qq.com>
2016-04-07 22:18:46 +0800

    Revert "调整了代码样式"
    This reverts commit ae32252340d17b87963dad637fe7164872fd76af.

--- jQuery-slide.js
+++ jQuery-slide.js
@@ -4,7 +4,7 @@ function Slider(node, config){
 		speed: 500, //滚动速度
 		perGroup: 1, //显示数量
 		slidePerView: 1, //每次滚动的数量
-		autoPlay: 0, //自动滚动的时间间隔，不大于0时关闭
+		autoPlay: 0, //自动滚动的时间间隔
 		loop: true, //是否循环播放
 		pagination: null, //分页器
 		pageClickable: true //分页器是否可点击
