84b57b6ad7ba0f225c562b607367729ffbefec69
林蛤蛤 <linzb93@qq.com>
2016-04-19 22:21:50 +0800

    优化代码，修复全屏滚动的bug。

--- dist/js/jQuery-slide.js
+++ dist/js/jQuery-slide.js
@@ -22,7 +22,6 @@ function Slide(node, config){
 	this.pageClickable = defaultPara.pageClickable,
 	this.fullPage = defaultPara.fullPage;
 
-
 	this.list = this.block.find('ul');
 	var _li = this.list.find('li');
 	this.liWidth = _li.width(),
@@ -32,39 +31,14 @@ function Slide(node, config){
 	_slideLength = Math.ceil((_length - this.perGroup) / this.slidePerView) + 1,
 	_timer = null,
 	_slideIndex = 0,
+	_pageDot = null,
 	_canShowPagination = this.pagination && this.perGroup === 1 && this.slidePerView === 1, //是否展示分页器
 	_that = this;
 
 //初始化
 var _init = function(){
-		//设定轮播样式
-		if(_that.fullPage){
-			var body = $("body");
-			_li.width(body.width());
-			_li.height(body.height());
-			if(_that.mode === 'horizontal'){
-				_that.liWidth = _li.width();
-				_that.list.width(_that.liWidth * _length);
-				_that.list.addClass('slide-horizontal');
-			}
-			else if(_that.mode === 'vertical'){
-				_that.liHeight = _li.height();
-				_that.list.height(_that.liHeight * _length);
-				_that.list.addClass('slide-vertical');
-			}
-		}
-		else{
-			if(_that.mode === 'horizontal'){
-				_that.block.width(_that.liWidth * _that.perGroup);
-				_that.list.width(_that.liWidth * _length);
-				_that.list.addClass('slide-horizontal');
-			}
-			else if(_that.mode === 'vertical'){
-				_that.block.height(_that.liHeight * _that.perGroup);
-				_that.list.height(_that.liHeight * _length);
-				_that.list.addClass('slide-vertical');
-			}
-		}
+		//初始化轮播样式
+		_setStyle();
 
 		//添加分页器
 		if(_canShowPagination){
@@ -122,6 +96,35 @@ var _init = function(){
 		_slideAnimation(-_delta * _that.slidePerView);
 	}
 
+	//初始化样式
+	var _setStyle = function(){
+		var body = $("body");
+		if(_that.mode === 'horizontal'){
+			if(_that.fullPage){
+				_li.width(body.width());
+				_li.height(body.height());
+				_that.liWidth = _li.width();
+			}
+			else{
+				_that.block.width(_that.liWidth * _that.perGroup);
+			}
+			_that.list.width(_that.liWidth * _length);
+			_that.list.addClass('slide-horizontal');
+		}
+		else if(_that.mode === 'vertical'){
+			if(_that.fullPage){
+				_li.width(body.width());
+				_li.height(body.height());
+				_that.liHeight = _li.height();
+			}
+			else{
+				_that.block.height(_that.liHeight * _that.perGroup);
+			}
+			_that.list.height(_that.liHeight * _length);
+			_that.list.addClass('slide-vertical');
+		}
+	};
+
 //初始化分页
 var _createPagination = function(){
 	var pageHtml = '';
@@ -129,7 +132,8 @@ var _createPagination = function(){
 		pageHtml += '<a href="javascript:;"></a>';
 	}
 	_that.pagination.append(pageHtml);
-	_that.pagination.find('a').eq(0).addClass('on');
+	_pageDot = _that.pagination.find('a');
+	_pageDot.eq(0).addClass('on');
 	//绑定分页器事件
 	if(_that.pageClickable){
 		_pageBind();
@@ -138,12 +142,12 @@ var _createPagination = function(){
 
 //分页器变换
 var _paginationChange = function(){
-	_that.pagination.find('a').eq(_slideIndex)._onlyClass('on');
+	_pageDot.eq(_slideIndex)._onlyClass('on');
 }
 
 //绑定分页器事件
 var _pageBind = function(){
-	_that.pagination.find('a').on('click', function(){
+	_pageDot.on('click', function(){
 		var dotIndex = $(this).index();
 		_that.slideTo(dotIndex);
 	});
